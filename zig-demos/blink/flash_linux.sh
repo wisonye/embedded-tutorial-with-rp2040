1d2cbe19074b09a497fae7634b8c25bd39117d36
Wison Ye <wisonye@gmail.com>
2023-10-27 12:46:24 +1300

    [ master ] - added 'zig-demos/blink' script and cmake configuration.

---
+++ zig-demos/blink/flash_linux.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo ">>>"
+    echo ">>> [ Usage ]"
+    echo ">>>"
+    echo ">>> 1. Unplug your pico board"
+    echo ">>> 2. Hold down the BOOTSEL button and re-plug your pico board for a few seconds"
+    echo ">>> 3. Run 'lsblk' or 'doas dmesg | tail' to confirm pico attached as USB device. If it doesn't 'dev/sdb1' then change it in the script."
+    echo ">>>"
+    echo ">>> doas ./flash_linux.sh DEMO_NAME_HERE"
+    echo ">>>"
+    echo ">>> [ Example ]"
+    echo ">>>"
+    echo ">>> doas ./flash_linux.sh zig-blink"
+    echo ">>>"
+
+    exit 0
+fi
+
+rm -rf /mnt/pico
+mkdir /mnt/pico
+mount /dev/sdb1 /mnt/pico
+cp -rvf "build/${1}.uf2" /mnt/pico
+umount /mnt/pico
---
+++ zig-demos/blink/flash_macos.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo ">>>"
+    echo ">>> [ Usage ]"
+    echo ">>>"
+    echo ">>> 1. Unplug your pico board"
+    echo ">>> 2. Hold down the BOOTSEL button and re-plug your pico board for a few seconds"
+    echo ">>>"
+    echo ">>> ./flash_macos.sh DEMO_NAME_HERE"
+    echo ">>>"
+    echo ">>> [ Example ]"
+    echo ">>>"
+    echo ">>> ./flash_macos.sh zig-blink"
+    echo ">>>"
+
+    exit 0
+fi
+
+cp -rvf "build/${1}.uf2" /Volumes/RPI-RP2/
